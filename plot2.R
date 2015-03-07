e36934bb02c1a7b16263abf4e64d95658805720f
Ana Lira <anarlira@hotmail.com>
2015-03-07 23:44:12 +0100

    plot2

---
+++ plot2.R
@@ -0,0 +1,11 @@
+data<-read.table("household_power_consumption.txt",sep=";",na.string="?",header=TRUE)
+plot2<-data[(data$Date=="1/2/2007" | data$Date=="2/2/2007" ),]
+
+plot2$DT<-strptime(paste(str_trim(p1$Date),str_trim(p1$Time),sep=" "),format="%d/%m/%Y %H:%M:%S")
+plot(plot2$DT,plot2$Global_active_power,pch=46,ylab="Global Active Power (kwatts)",xlab="")
+lines(plot2$DT,plot2$Global_active_power)
+
+png("plot2.png",width = 480, height = 480, units = "px")
+plot(plot2$DT,plot2$Global_active_power,pch=46,ylab="Global Active Power (kwatts)",xlab="")
+lines(plot2$DT,plot2$Global_active_power)
+dev.off()
