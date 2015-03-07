0b91cdb91324b80c9426b65c6e77ce89aef04bc8
Ana Lira <anarlira@hotmail.com>
2015-03-07 21:33:07 +0100

    Exploratory Data Analysis Project 1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data<-read.table("household_power_consumption.txt",sep=";",na.string="?",header=TRUE)
+plot1<-data[(data$Date=="1/2/2007" | data$Date=="2/2/2007" ),]
+
+hist(plot1$Global_active_power,breaks=14,col="red",xlab="Global Active Power (kwatts)",main="Global Active Power")
+
+png("plot1.png",width = 480, height = 480, units = "px")
+hist(p1$Global_active_power,breaks=14,col="red",xlab="Global Active Power (kwatts)",main="Global Active Power")
+dev.off()
